057e85cb729f63114fb9862916fdc3e187c80d21
Ruslanbek Normurodov <aksirus89@gmail.com>
2023-05-03 13:15:33 +0500

    feat: implement read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs  = require('fs');
+const readableStream = fs.createReadStream('01-read-file/text.txt');
+
+readableStream.on('data', (data) => {
+    console.log(data.toString())});
+    
+readableStream.on('end', ()=>{console.log('reading finished!')});    
