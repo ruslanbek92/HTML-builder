67a37e1718e22a610b69e1c9aa4f803ed91c866e
Ruslanbek Normurodov <aksirus89@gmail.com>
2023-05-04 17:39:00 +0500

    feat: implement files in folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir('./03-files-in-folder/secret-folder',{withFileTypes:true}, (err, files) => {
+    if(err) throw err; 
+    const arr = []
+    files = files.filter(el => el.isFile());
+    files.forEach(el =>{
+        let obj = {};
+        obj.fileName=el.name.slice(0, el.name.lastIndexOf('.'));
+        obj.extension=el.name.slice(el.name.lastIndexOf('.')+1);
+        arr.push(obj);
+    });
+
+    for (let i = 0; i < files.length; i++) {
+       fs.stat(`./03-files-in-folder/secret-folder/${files[i].name}`,(err, stats)=>{
+        arr[i].size = stats.size;
+        console.log(`${arr[i].fileName}-${arr[i].extension}-${arr[i].size}`)
+       }); 
+    }
+} );
