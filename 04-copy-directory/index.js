d7b74d9731fd1541f251e8cadfeb6b7788b72c53
Ruslanbek Normurodov <aksirus89@gmail.com>
2023-05-05 21:37:47 +0500

    feat:implement copy-directory task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -9,7 +9,23 @@ fs.mkdir('./04-copy-directory/copy-files', {recursive:true}, (err)=>{
              await fs.promises.unlink(`${dir}/${files[i]}`);
             }
         }
-           deleteFiles('./04-copy-directory/copy-files');
+           deleteFiles('./04-copy-directory/copy-files')
+           .then(() => {
+            fs.promises.readdir(`./04-copy-directory/files`)
+            .then((files)=>{
+            async function copyFiles(){
+                for(let i = 0; i < files.length; i++) {
+                    await fs.promises.copyFile(`./04-copy-directory/files/${files[i]}`,`./04-copy-directory/copy-files/${files[i]}`); 
+                }
+            }
+            copyFiles(); 
+            })
+            .catch(()=>{console.log('error in reading files')})
+            
+           }
+           
+           );
+        
      }
     )
     .catch(()=>{console.log('error')});
